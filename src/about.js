959d614e37d9ec3efaf2a2463d2192fb6de0f87a
Artem-1-1 <aleksejchukartem@gmail.com>
2025-11-11 19:53:43 +0300

    Fixing 4 -> 2 indents in 3 files.

--- src/about.js
+++ src/about.js
@@ -1,14 +1,14 @@
 import { content } from "./index.js";
 
 function aboutRender() {
-    const title = document.createElement('h1');
-    title.textContent = 'About';
-    const sep = document.createElement('hr');
-    const text = document.createElement('p');
-    text.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit sit temporibus a officiis! Quae ex culpa nobis distinctio soluta modi molestiae voluptatem esse! Quia, hic. Atque dolorum eum debitis. Voluptatem!'
-    content.appendChild(title);
-    content.appendChild(sep);
-    content.appendChild(text);
+  const title = document.createElement('h1');
+  title.textContent = 'About';
+  const sep = document.createElement('hr');
+  const text = document.createElement('p');
+  text.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit sit temporibus a officiis! Quae ex culpa nobis distinctio soluta modi molestiae voluptatem esse! Quia, hic. Atque dolorum eum debitis. Voluptatem!'
+  content.appendChild(title);
+  content.appendChild(sep);
+  content.appendChild(text);
 }
 
 export { aboutRender };
--- src/home.js
+++ src/home.js
@@ -1,18 +1,18 @@
 import { content } from "./index.js";
 
 function homeRender() {
-    const welcome = document.createElement('p');
-    welcome.id = 'welcome';
-    welcome.textContent = 'Welcome!';
-    const title = document.createElement('h1');
-    title.textContent = 'Restaurant';
-    const sep = document.createElement('hr');
-    const text = document.createElement('p');
-    text.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque dicta, commodi nemo accusamus, libero, harum necessitatibus aut nobis voluptate sunt nihil explicabo. Quasi, fuga cupiditate?';
-    content.appendChild(welcome)
-    content.appendChild(title);
-    content.appendChild(sep);
-    content.appendChild(text);
+  const welcome = document.createElement('p');
+  welcome.id = 'welcome';
+  welcome.textContent = 'Welcome!';
+  const title = document.createElement('h1');
+  title.textContent = 'Restaurant';
+  const sep = document.createElement('hr');
+  const text = document.createElement('p');
+  text.textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque dicta, commodi nemo accusamus, libero, harum necessitatibus aut nobis voluptate sunt nihil explicabo. Quasi, fuga cupiditate?';
+  content.appendChild(welcome)
+  content.appendChild(title);
+  content.appendChild(sep);
+  content.appendChild(text);
 }
 
 export { homeRender};
